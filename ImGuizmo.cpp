7cc730f8774db7cf392a8bb7de4b939f72cc15d3
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-06-20 20:45:45 +0300

    Update ImGuizmo.cpp

--- ImGuizmo.cpp
+++ ImGuizmo.cpp
@@ -729,10 +729,10 @@ namespace ImGuizmo
    static Context gContext;
 
    static const vec_t directionUnary[3] = { makeVect(1.f, 0.f, 0.f), makeVect(0.f, 1.f, 0.f), makeVect(0.f, 0.f, 1.f) };
-   static const ImU32 directionColor[3] = { 0xFF0000AA, 0xFF00AA00, 0xFFAA0000 };
+   static const ImU32 directionColor[3] = { 0xFF0000AA, 0xFFAA0000, 0xFF00AA00 };
 
    // Alpha: 100%: FF, 87%: DE, 70%: B3, 54%: 8A, 50%: 80, 38%: 61, 12%: 1F
-   static const ImU32 planeColor[3] = { 0x610000AA, 0x6100AA00, 0x61AA0000 };
+   static const ImU32 planeColor[3] = { 0x610000AA, 0x61AA0000, 0x6100AA00 };
    static const ImU32 selectionColor = 0x8A1080FF;
    static const ImU32 inactiveColor = 0x99999999;
    static const ImU32 translationLineColor = 0xAAAAAAAA;
